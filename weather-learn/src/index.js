4d00421cac1812374d55ca96b524af217c73d48d
Jed Cheng <jedlawrencecheng@gmail.com>
2021-05-31 01:56:10 -0400

    added ternary condition for season and icons

--- weather-learn/src/index.js
+++ weather-learn/src/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    //same with this.state inside constructor
+    state = { lat: null, errorMessage: ''};
 
-        this.state = { lat: null };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({lat: position.coords.latitude});
-            },
-            (err) => console.log(err)
+            (position) => this.setState({lat: position.coords.latitude}),
+            (err) => this.setState({errorMessage: err.message})
         );
     }
 
     render() {
-        return <div>Latitude: {this.state.lat} </div>;
+        
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat = {this.state.lat} />
+        }
+
+        return <div>Loading!</div>
     }
 }
 
